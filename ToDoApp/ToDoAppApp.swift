58640ab6a33eb22e8a481bd024f00034edd4cd0e
vasuchampaneria <vasu4802@outlook.com>
2025-11-24 23:21:56 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Vasu Champaneria on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
